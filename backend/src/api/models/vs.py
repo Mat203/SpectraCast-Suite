0eaf8096aaaa724d6e3ccd173c41bd09499c7061
Mat203 <matvey.talalevskiy@gmail.com>
2026-04-20 15:09:36 +0300

    vs api setup

--- backend/src/api/models/vs.py
+++ backend/src/api/models/vs.py
@@ -1,14 +1,25 @@
 from pydantic import BaseModel
-from typing import List, Optional
+from typing import List
+
+class StyleListResponse(BaseModel):
+    styles: List[str]
 
 class GeneratePlotRequest(BaseModel):
     file_id: str
-    is_cleaned: bool = False
-    x_col: Optional[str] = None
-    y_cols: List[str]
-    chart_type: str
-    style_filename: Optional[str] = None
+    style_name: str
+    x: str
+    y: str
+    plot_type: str
+    output_filename: str
 
 class GeneratePlotResponse(BaseModel):
     status: str
-    plot_base64: str
+    plot_path: str
+
+class StandardizeCodeRequest(BaseModel):
+    raw_code: str
+    style_name: str
+
+class StandardizeCodeResponse(BaseModel):
+    status: str
+    cleaned_code: str
